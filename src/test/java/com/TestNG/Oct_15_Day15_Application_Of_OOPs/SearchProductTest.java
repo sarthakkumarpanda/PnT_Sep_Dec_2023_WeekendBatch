efa74045296d5865220c388c4d884fd554e837e7
sarthakkumarpanda <sarthak.matrix@gmail.com>
2023-10-21 14:44:17 -0400

    application of OOPS

---
+++ src/test/java/com/TestNG/Oct_15_Day15_Application_Of_OOPs/SearchProductTest.java
@@ -0,0 +1,49 @@
+package com.TestNG.Oct_15_Day15_Application_Of_OOPs;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SearchProductTest extends TestBase{
+
+public SearchProductTest() throws Exception {
+		super();
+		
+	}
+
+public WebDriver driver;
+	
+	@BeforeMethod
+	public void registerSetup() {
+		driver = initializeBrowserAndOpenApplication("chrome");
+	}
+	
+	
+	@Test(priority=1)
+	public void verifySearchWithValidProduct() {
+		driver.findElement(By.name("search")).sendKeys("HP");
+		driver.findElement(By.cssSelector("button.btn.btn-default.btn-lg")).click();
+		Assert.assertTrue(driver.findElement(By.linkText("HP LP3065")).isDisplayed());
+	}
+	@Test(priority=2)
+	public void verifySearchWithInvalidProduct() {
+		driver.findElement(By.name("search")).sendKeys("DELL");
+		driver.findElement(By.cssSelector("button.btn.btn-default.btn-lg")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//p[text() = 'There is no product that matches the search criteria.']")).isDisplayed());
+	}
+	
+	@Test(priority=3)
+	public void verifySearchWithNoProduct() {
+		driver.findElement(By.cssSelector("button.btn.btn-default.btn-lg")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//p[text() = 'There is no product that matches the search criteria.']")).isDisplayed());
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+}
---
+++ src/test/java/com/TestNG/Oct_15_Day15_Application_Of_OOPs/TestBase.java
@@ -0,0 +1,43 @@
+package com.TestNG.Oct_15_Day15_Application_Of_OOPs;
+
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestBase {
+
+	public WebDriver driver;
+	public Properties prop;
+	public FileInputStream ip;
+	
+	
+	//I will create a Constructor of this TestBase and write the Properties file code in the constructor and I will use that Constructor
+	// in all the Child classes
+	
+	public TestBase() throws Exception {
+		prop = new Properties();
+		ip = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\com\\TestNG\\Oct_15_Day15_Application_Of_OOPs\\config.properties");
+		prop.load(ip);
+	}
+
+	public WebDriver initializeBrowserAndOpenApplication(String browserName) {
+		
+		if(browserName.equals("chrome")) {
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		}else if(browserName.equals("firefox")) {
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+		}
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://tutorialsninja.com/demo");
+		
+		return driver;
+	}
+	
+}
